e745f99c60985d47d47d569166fd335b662c870d
Ally Jarjour <allyjarjour@gmail.com>
2021-11-24 13:20:11 -0500

    Add main.css for global font family

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,14 @@
 /** @jsx jsx */
+import "../css/main.css";
 import { jsx } from "theme-ui";
 import { ThemeProvider } from "theme-ui";
 import theme from "../theme";
-import Nav from "../src/components/nav";
+import Nav from "../src/components/Nav";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <div>
+      <div className="daily-standup-app">
         <Nav />
         <Component {...pageProps} />
       </div>
