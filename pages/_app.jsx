2754d9a09d812cde3e5735cdbb518af49289e1ad
Ally Jarjour <allyjarjour@gmail.com>
2021-12-05 10:24:59 -0500

    Add head to app with tile and favicon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,10 +5,15 @@ import { jsx } from "theme-ui";
 import { ThemeProvider } from "theme-ui";
 import theme from "../theme";
 import Nav from "../src/components/Nav";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Standup Summary</title>
+        <link rel="shortcut icon" href="pencil.png" />
+      </Head>
       <div className="daily-standup-app">
         <Nav />
         <Component {...pageProps} />
