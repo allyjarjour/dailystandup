0dc1900cccf2ee7679fdfaa0ee4021d7197d3b5c
Ally Jarjour <allyjarjour@gmail.com>
2022-05-25 23:34:50 -0400

    Update login url

--- src/components/MustBeLoggedInMessage.tsx
+++ src/components/MustBeLoggedInMessage.tsx
@@ -16,7 +16,12 @@ export default function MustBeLoggedInMessage() {
           Daily Standup
         </Text>
         <Image src="https://static.thenounproject.com/png/79533-200.png" />
-        <Button mt={2} onClick={() => signIn("github", { callbackUrl: "/" })}>
+        <Button
+          mt={2}
+          onClick={() =>
+            signIn(undefined, { callbackUrl: process.env.NEXTAUTH_URL })
+          }
+        >
           Sign in with GitHub
         </Button>
       </Flex>
